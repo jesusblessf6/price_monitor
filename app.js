d6c0a11dd613301375696823b8efa1c669ef7aa2
wing <jesusblessf6@gmail.com>
2014-04-01 17:49:57 +0800

    update

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+//app.get('/', routes.index);
+routes(app);
 
 var server = http.createServer(app);
 var io = socket.listen(app);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+
+//exports.index = function(req, res){
+//  res.render('index', { title: 'Express' });
+//};
+
+module.exports = function(app){
+	app.get("/", function(req, res){
+		res.render('index', { title: 'Express' });
+	});
+
+	app.get("/myItems", function(req, res){
+		res.render('myItems', {title : '我关注的商品'});
+	});
 };
